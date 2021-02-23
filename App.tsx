cf2f12f280bc5b52c8faea64c02c7a7af03437c3
apatryk <patrykantecki@gmail.com>
2021-02-23 02:45:39 +0100

    ...

--- App.tsx
+++ App.tsx
@@ -6,7 +6,6 @@ const App = () => {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [items, setItems] = useState([]);
-  var arr = [];
   // Note: the empty deps array [] means
   // this useEffect will run once
   // similar to componentDidMount()
@@ -17,24 +16,19 @@ const App = () => {
     // Selecting the first 3 records in Grid view:
     
     view: "Grid view"
-  }).eachPage(function page(records, fetchNextPage) {
+  }).eachPage(function page(records: Array<any>, fetchNextPage) {
     // This function (`page`) will get called for each page of records.
-
+    setItems(records.map(record => {return record.fields}));
     records.forEach(function(record) {
         var ajdi = record.id
         base('Table 1').find(ajdi, function(err, record) {
           if (err) { console.error(err); return; }
           for(var i = 1; i< Object.keys(record.fields).length; i++){
-          var d1 = new Date(record.get("Date" + i));
+          var d1 = new Date(record.get("date" + i));
           var d2 = new Date();
           var roznica = d1.getTime() - d2.getTime();
           var days = Math.ceil(roznica / (1000 * 3600 * 24));
           if(Math.sign(days) == 1){
-            var ulica = record.get("Name");
-            var key = ulica;
-            var obj = {};
-            obj[key] = days;
-            arr.push(obj);
             console.log('wywóz', ulica, days, arr);
           }
         }
@@ -51,16 +45,19 @@ const App = () => {
     if (err) { console.error(err); return; }
     setIsLoaded(true);
     setItems(arr);
+    console.log(arr)
   });
 }, [])
 
   if (error) {
     return <div>Error: {error.message}</div>;
   } else if (!isLoaded) {
-    return <div>Loading...</div>;
+    return <div>Ładowanie danych...</div>;
   } else {
+  console.log(items)
     return (
-      <Text>{items}</Text>
+      // items.map(item => <Text>{JSON.stringify(item)}</Text>)
+      <Text>{items.map(item => item.name + item.date)}</Text>
     );
   }
 }
